14413728a77fa76b6454cd4ce8d7f5fa2395149a
Marie Rigal <marierigal4@gmail.com>
2023-01-22 17:30:59 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# USM WordPress plugin
+
+> WordPress plugin to add custom post types
+
+## Installation
+
+1. Download this project in `zip` format
+2. In your WordPress admin, go to `Plugins > Add New`
+3. Click on `Upload Plugin`
+4. Select the `zip` file you just downloaded
+5. Click on `Install Now`
+6. Click on `Activate Plugin`
+7. You're done!
---
+++ usm-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * USM Plugin
+ *
+ * @package      USMPlugin
+ * @author       Marie Rigal
+ * @license      GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:  USM Plugin
+ * Author:       Marie Rigal
+ * Description:  Custom post types for the USM website
+ * Author URI:   https://github.com/marierigal
+ * Version:      1.0.0
+ * License:      GNU General Public License v3
+ * License URI:  http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:  usm-plugin
+ * Domain Path:  /languages
+ */
