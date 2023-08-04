b4630e5c8514aaaee4289d3aeee7335a783eb7c1
Marie Rigal <marierigal4@gmail.com>
2023-08-04 17:39:49 +0200

    style: fix tab spaces

--- php/players/player-admin-table.php
+++ php/players/player-admin-table.php
@@ -2,74 +2,73 @@
 global $pagenow;
 
 // Add the custom columns to the usmp_player post type:
-function usmp_player_manage_columns( $columns )
-{
-    // save date to the variable
-    $date = $columns['date'];
-    // unset the 'date' column
-    unset( $columns['date'] );
-    // unset any column when necessary
-    unset( $columns['stats'] );
+function usmp_player_manage_columns($columns) {
+  // save date to the variable
+  $date = $columns['date'];
+  // unset the 'date' column
+  unset($columns['date']);
+  // unset any column when necessary
+  unset($columns['stats']);
 
-    // add your column as new array element and give it table header text
-    $columns['usmp_player_position'] = __('Poste', 'usm-plugin');
+  // add your column as new array element and give it table header text
+  $columns['usmp_player_position'] = __('Poste', 'usm-plugin');
 
-    $columns['date'] = $date; // set the 'date' column again, after the custom column
+  $columns['date'] = $date; // set the 'date' column again, after the custom column
 
-    return $columns;
+  return $columns;
 }
 
-function usmp_player_set_sortable_columns( $columns )
+function usmp_player_set_sortable_columns($columns)
 {
-    $columns['usmp_player_position'] = 'usmp_player_position';
-    return $columns;
+  $columns['usmp_player_position'] = 'usmp_player_position';
+  return $columns;
 }
 
-function usmp_player_populate_custom_columns( $column, $post_id )
+function usmp_player_populate_custom_columns($column, $post_id)
 {
-    switch ( $column ) {
-        case 'usmp_player_position':
-            $position = get_post_meta($post_id, 'usmp_player_position', true);
-            echo USM_PLAYER_POSITIONS[$position] ?? USM_STAFF_POSITIONS[$position] ?? '-';
-            break;
-        default:
-            break;
-    }
+  switch ($column) {
+    case 'usmp_player_position':
+      $position = get_post_meta($post_id, 'usmp_player_position', true);
+      echo USM_PLAYER_POSITIONS[$position] ?? USM_STAFF_POSITIONS[$position] ?? '-';
+      break;
+
+    default:
+      break;
+  }
 }
 
-function usmp_player_sort_custom_columns_query( $query )
+function usmp_player_sort_custom_columns_query($query)
 {
-    $orderby = $query->get( 'orderby' );
+  $orderby = $query->get('orderby');
 
-    if ( 'usmp_player_position' == $orderby ) {
+  if ('usmp_player_position' == $orderby) {
 
-        $meta_query = array(
-            'relation' => 'OR',
-            array(
-                'key' => 'usmp_player_position',
-                'compare' => 'NOT EXISTS',
-            ),
-            array(
-                'key' => 'usmp_player_position',
-            ),
-        );
+    $meta_query = array(
+      'relation' => 'OR',
+      array(
+        'key' => 'usmp_player_position',
+        'compare' => 'NOT EXISTS',
+      ),
+      array(
+        'key' => 'usmp_player_position',
+      ),
+    );
 
-        $query->set( 'meta_query', $meta_query );
-        $query->set( 'orderby', 'meta_value' );
-    }
+    $query->set('meta_query', $meta_query);
+    $query->set('orderby', 'meta_value');
+  }
 }
 
-if ( is_admin() && 'edit.php' == $pagenow && 'usmp_player' == ($_GET['post_type'] ?? '') ) {
+if (is_admin() && 'edit.php' == $pagenow && 'usmp_player' == ($_GET['post_type'] ?? '')) {
+  // manage columns
+  add_filter('manage_usmp_player_posts_columns', 'usmp_player_manage_columns');
 
-    // manage colunms
-    add_filter( 'manage_usmp_player_posts_columns', 'usmp_player_manage_columns' );
+  // make columns sortable
+  add_filter('manage_edit-usmp_player_sortable_columns', 'usmp_player_set_sortable_columns');
 
-    // make columns sortable
-    add_filter( 'manage_edit-usmp_player_sortable_columns', 'usmp_player_set_sortable_columns' );
+  // populate column cells
+  add_action('manage_usmp_player_posts_custom_column', 'usmp_player_populate_custom_columns', 10, 2);
 
-    // populate column cells
-    add_action( 'manage_usmp_player_posts_custom_column', 'usmp_player_populate_custom_columns', 10, 2 );
-
-    // set query to sort
-    add_action( 'pre_get_posts', 'usmp_player_sort_custom_columns_query' );
+  // set query to sort
+  add_action('pre_get_posts', 'usmp_player_sort_custom_columns_query');
 }
--- php/sponsors/sponsor-admin-table.php
+++ php/sponsors/sponsor-admin-table.php
@@ -2,74 +2,70 @@
 global $pagenow;
 
 // Add the custom columns to the usmp_sponsor post type:
-function usmp_sponsor_manage_columns( $columns )
-{
-    // save date to the variable
-    $date = $columns['date'];
-    // unset the 'date' column
-    unset( $columns['date'] );
-    // unset any column when necessary
-    unset( $columns['stats'] );
+function usmp_sponsor_manage_columns($columns) {
+  // save date to the variable
+  $date = $columns['date'];
+  // unset the 'date' column
+  unset($columns['date']);
+  // unset any column when necessary
+  unset($columns['stats']);
 
-    // add your column as new array element and give it table header text
-    $columns['usmp_sponsor_engagement_level'] = __('Niveau d\'engagement', 'usm-plugin');
+  // add your column as new array element and give it table header text
+  $columns['usmp_sponsor_engagement_level'] = __('Niveau d\'engagement', 'usm-plugin');
 
-    $columns['date'] = $date; // set the 'date' column again, after the custom column
+  $columns['date'] = $date; // set the 'date' column again, after the custom column
 
-    return $columns;
+  return $columns;
 }
 
-function usmp_sponsor_set_sortable_columns( $columns )
-{
-    $columns['usmp_sponsor_engagement_level'] = 'usmp_sponsor_engagement_level';
-    return $columns;
+function usmp_sponsor_set_sortable_columns($columns) {
+  $columns['usmp_sponsor_engagement_level'] = 'usmp_sponsor_engagement_level';
+  return $columns;
 }
 
-function usmp_sponsor_populate_custom_columns( $column, $post_id )
-{
-    switch ( $column ) {
-        case 'usmp_sponsor_engagement_level':
-            $position = get_post_meta($post_id, 'usmp_sponsor_engagement_level', true);
-            echo USM_SPONSOR_ENGAGEMENT_LEVELS[$position] ?? '-';
-            break;
-        default:
-            break;
-    }
+function usmp_sponsor_populate_custom_columns($column, $post_id) {
+  switch ($column) {
+    case 'usmp_sponsor_engagement_level':
+      $position = get_post_meta($post_id, 'usmp_sponsor_engagement_level', true);
+      echo USM_SPONSOR_ENGAGEMENT_LEVELS[$position] ?? '-';
+      break;
+
+    default:
+      break;
+  }
 }
 
-function usmp_sponsor_sort_custom_columns_query( $query )
-{
-    $orderby = $query->get( 'orderby' );
+function usmp_sponsor_sort_custom_columns_query($query) {
+  $orderby = $query->get('orderby');
 
-    if ( 'usmp_sponsor_engagement_level' == $orderby ) {
+  if ('usmp_sponsor_engagement_level' == $orderby) {
 
-        $meta_query = array(
-            'relation' => 'OR',
-            array(
-                'key' => 'usmp_sponsor_engagement_level',
-                'compare' => 'NOT EXISTS',
-            ),
-            array(
-                'key' => 'usmp_sponsor_engagement_level',
-            ),
-        );
+    $meta_query = array(
+      'relation' => 'OR',
+      array(
+        'key' => 'usmp_sponsor_engagement_level',
+        'compare' => 'NOT EXISTS',
+      ),
+      array(
+        'key' => 'usmp_sponsor_engagement_level',
+      ),
+    );
 
-        $query->set( 'meta_query', $meta_query );
-        $query->set( 'orderby', 'meta_value' );
-    }
+    $query->set('meta_query', $meta_query);
+    $query->set('orderby', 'meta_value');
+  }
 }
 
-if ( is_admin() && 'edit.php' == $pagenow && 'usmp_sponsor' == ($_GET['post_type'] ?? '') ) {
+if (is_admin() && 'edit.php' == $pagenow && 'usmp_sponsor' == ($_GET['post_type'] ?? '')) {
+  // manage columns
+  add_filter('manage_usmp_sponsor_posts_columns', 'usmp_sponsor_manage_columns');
 
-    // manage colunms
-    add_filter( 'manage_usmp_sponsor_posts_columns', 'usmp_sponsor_manage_columns' );
+  // make columns sortable
+  add_filter('manage_edit-usmp_sponsor_sortable_columns', 'usmp_sponsor_set_sortable_columns');
 
-    // make columns sortable
-    add_filter( 'manage_edit-usmp_sponsor_sortable_columns', 'usmp_sponsor_set_sortable_columns' );
+  // populate column cells
+  add_action('manage_usmp_sponsor_posts_custom_column', 'usmp_sponsor_populate_custom_columns', 10, 2);
 
-    // populate column cells
-    add_action( 'manage_usmp_sponsor_posts_custom_column', 'usmp_sponsor_populate_custom_columns', 10, 2 );
-
-    // set query to sort
-    add_action( 'pre_get_posts', 'usmp_sponsor_sort_custom_columns_query' );
+  // set query to sort
+  add_action('pre_get_posts', 'usmp_sponsor_sort_custom_columns_query');
 }
